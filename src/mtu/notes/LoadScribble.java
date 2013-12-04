34c675d5ee6339e102d8403f965779ac788ff816
fatalaim <fatalsilence1@hotmail.com>
2013-12-04 15:11:17 -0500

    Fixed a null pointer exception.
    Fixed activity heirarchy so back buttons work.

--- src/mtu/notes/LoadScribble.java
+++ src/mtu/notes/LoadScribble.java
@@ -45,18 +45,21 @@ public class LoadScribble extends Activity {
 		if(folder.exists())
 		{
 			String[] folders = folder.list();
-			for(String name : folders)
+			if(folders != null)
 			{
-				category = new File(folder + "/" + name);
-				if(category.isDirectory() && list.contains(name))
+				for(String name : folders)
 				{
-					for(String names : category.list())
+					category = new File(folder + "/" + name);
+					if(category.isDirectory() && list.contains(name))
 					{
-						note = new File(folder + "/" + name + "/" + names);
-						if(note.isDirectory())
+						for(String names : category.list())
 						{
-							notes.add(names);
-							path.add(folder + "/" + name + "/" + names);
+							note = new File(folder + "/" + name + "/" + names);
+							if(note.isDirectory())
+							{
+								notes.add(names);
+								path.add(folder + "/" + name + "/" + names);
+							}
 						}
 					}
 				}
@@ -70,6 +73,34 @@ public class LoadScribble extends Activity {
 		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 		// Apply the adapter to the spinner
 		spinner.setAdapter(adapter);
+		setupActionBar();
+	}
+	
+	/**
+	 * Set up the {@link android.app.ActionBar}, if the API is available.
+	 */
+	@TargetApi(Build.VERSION_CODES.HONEYCOMB)
+	private void setupActionBar() {
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+			getActionBar().setDisplayHomeAsUpEnabled(true);
+		}
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case android.R.id.home:
+			// This ID represents the Home or Up button. In the case of this
+			// activity, the Up button is shown. Use NavUtils to allow users
+			// to navigate up one level in the application structure. For
+			// more details, see the Navigation pattern on Android Design:
+			//
+			// http://developer.android.com/design/patterns/navigation.html#up-vs-back
+			//
+			NavUtils.navigateUpFromSameTask(this);
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
 	}
 
 	@Override
